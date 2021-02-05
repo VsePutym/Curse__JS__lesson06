cdd91fd61344a0a97dc6874c8e382d55cc624017
Vse Putyom <bertraid777@gmail.com>
2021-02-05 03:52:12 +0300

    feat(script.js): new function

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 'use strict';
-
 function getRandomNumber(max) {
-    return Math.floor(Math.random()* Math.floor(max));
-}
-let result = getRandomNumber(100);
-
-let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-function start() {
-    let userNumber = +prompt('guess the number from 0 to 100');
-    let num = result;
-
-    if (isNumber(userNumber)) {
-        if (userNumber === num) {
-            alert('congratulations, you guessed it');
-        } else if (userNumber > num) {
-            if (confirm('Try again it more')) {
-                start();
-            } else {
-                alert('game over');
-            }
-        } else if (userNumber < num) {
-            if (confirm('Try again it less')) {
-                start();
-            } else {
-                alert('game over');
-            }
-        }
-    } else {
-        confirm('is not a number');
-            start();
-        }
-        console.log(num);
-    }
+    return Math.floor(Math.random() * Math.floor(max));
     
+}
+//! function wrapping
+function start() {
+    let isNumber = function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+    
+    let num = getRandomNumber(100);
+//? function main internal
+    function newStart() {
+        let userNumber = prompt('guess the number from 0 to 100');
+        if (isNumber(userNumber)) {
+            if (userNumber === num) {
+                alert('congratulations, you guessed it');
+            } else if (userNumber > num) {
+                if (confirm('Try again it more')) {
+                    newStart();
+                } else {
+                    alert('game over');
+                }
+            } else if (userNumber < num) {
+                if (confirm('Try again it less')) {
+                    newStart();
+                } else {
+                    newStart('game over');
+                }
+            }
+        } else {
+            confirm('is not a number');
+            newStart();
+        }
 
+    }
+    console.dir(newStart);
+    newStart();
+}
 start();
